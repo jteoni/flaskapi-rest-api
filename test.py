b689300b0e6dc384b0c89bfb9b15a4bfb232c0a4
jbtestoni <jbn.testoni@gmail.com>
2024-06-06 15:56:55 -0300

    Create API

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+data = [{"name": "Flask REST API", "views": 20000, "likes": 19928},
+        {"name": "Fast API with Python", "views": 50000, "likes": 49328},
+        {"name": "Fullstack project: React + Python", "views": 100000, "likes": 99958}]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
